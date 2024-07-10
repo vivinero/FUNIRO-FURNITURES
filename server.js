f4132d0c7a5048974ec2e579ae55f96326e31711
vivinero <viviannzemeke@gmail.com>
2024-07-10 13:03:22 +0100

    updated

--- server.js
+++ server.js
@@ -4,20 +4,6 @@ const userRouter = require("./routers/userRouter")
 const categoryRouter = require("./routers/categoryRouter")
 const productRouter = require("./routers/productRouter")
 
-const app = express()
-app.use(express.json())
-app.use(userRouter)
-app.use(categoryRouter)
-app.use(productRouter)
-
-app.listen(process.env.port,()=>{
-    console.log(`Server is Listening on port: ${process.env.port}`);
-})const express = require("express")
-require('./config/config')
-const userRouter = require("./routers/userRouter")
-const categoryRouter = require("./routers/categoryRouter")
-const productRouter = require("./routers/productRouter")
-
 
 //create express instance
 const app = express()
