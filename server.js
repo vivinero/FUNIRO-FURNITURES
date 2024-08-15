f35695c85237d10d4457049826c4700ff4212124
AmehChenemi <amehlove147@gmail.com>
2024-08-15 13:36:20 +0100

    updates

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors("*"))
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 
 app.get('/', (req, res) => {
