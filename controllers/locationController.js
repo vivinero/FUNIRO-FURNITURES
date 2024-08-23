ea9198843626b225bc1d0fe3f4348da4a866c2cd
AdekunleM <adekunlemichael1319@gmail.com>
2024-08-23 23:53:45 +0100

    updated location

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -7,7 +7,7 @@ exports.getAllCountries = (req, res) => {
 };
 
 // Get states by country code
-exports.getStatesByCountry = (req, res) => {
+exports.getStatesByCountryS = (req, res) => {
   const { countryCode } = req.params;
   const states = State.getStatesOfCountry(countryCode);
   if (states.length === 0) {
@@ -18,8 +18,34 @@ exports.getStatesByCountry = (req, res) => {
   res.json(states);
 };
 
+
+exports.getStatesByCountry = (req, res) => {
+  const { countryName } = req.params;
+
+  // Find the country using the name
+  const country = Country.getAllCountries().find(
+    (country) => country.name.toLowerCase() === countryName.toLowerCase()
+  );
+
+  if (!country) {
+    return res.status(404).json({
+      message: `Country with the name: ${countryName} not found`,
+    });
+  }
+
+  const states = State.getStatesOfCountry(country.isoCode);
+  if (states.length === 0) {
+    return res.status(404).json({
+      message: `No states found for the country: ${countryName}`,
+    });
+  }
+
+  res.json(states);
+};
+
+
 // Get cities by state code
-exports.getCitiesByState = (req, res) => {
+exports.getCitiesByStates = (req, res) => {
     const { countryCode, isoCode } = req.params;
     const cities = City.getCitiesOfState(countryCode, isoCode);
     if (cities.length === 0) {
@@ -29,3 +55,41 @@ exports.getCitiesByState = (req, res) => {
     }
     res.json(cities);
 };
+
+
+exports.getCitiesByState = (req, res) => {
+    const { countryName, stateName } = req.params;
+
+    // Find the country using the name
+    const country = Country.getAllCountries().find(
+        (country) => country.name.toLowerCase() === countryName.toLowerCase()
+    );
+
+    if (!country) {
+        return res.status(404).json({
+            message: `Country with the name: ${countryName} not found`,
+        });
+    }
+
+    // Find the state using the name and the country isoCode
+    const state = State.getStatesOfCountry(country.isoCode).find(
+        (state) => state.name.toLowerCase() === stateName.toLowerCase()
+    );
+
+    if (!state) {
+        return res.status(404).json({
+            message: `State with the name: ${stateName} not found in ${countryName}`,
+        });
+    }
+
+    const cities = City.getCitiesOfState(country.isoCode, state.isoCode);
+
+    if (cities.length === 0) {
+        return res.status(404).json({
+            message: `No cities found for the state: ${stateName}`,
+        });
+    }
+
+    res.json(cities);
+};
+
