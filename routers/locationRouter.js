380caf589c0ccc6c83feb296f54c3387470ed4c7
AdekunleM <adekunlemichael1319@gmail.com>
2024-08-24 17:24:10 +0100

    search by product name added to the sort by

--- routers/locationRouter.js
+++ routers/locationRouter.js
@@ -3,10 +3,9 @@ const locationController = require('../controllers/locationController');
 const router = express.Router();
 
 router.get('/countries', locationController.getAllCountries);
-// router.get('/countries/:countryCode/states', locationController.getStatesByCountry);
+
 router.get('/countries/:countryName/states', locationController.getStatesByCountry);
 
-//router.get('/countries/:countryName/states/:isoCode/cities', locationController.getCitiesByState);
 router.get('/countries/:countryName/states/:stateName/cities', locationController.getCitiesByState);
 
 
