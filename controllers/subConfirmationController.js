c3cd09b8015895dec586ee5d0fa72561a805468c
AmehChenemi <amehlove147@gmail.com>
2024-08-27 16:32:14 +0100

    updated the confirmation subscription file

--- controllers/subConfirmationController.js
+++ controllers/subConfirmationController.js
@@ -1,27 +1,42 @@
 const jwt = require('jsonwebtoken');
 const Subscriber = require('../models/subscriptionModel.js'); 
-require('dotenv').config()
+require('dotenv').config();
 
-const secretKey = process.env.JWT_SECRET
+const secretKey = process.env.JWT_SECRET;
 
 const confirmSubscription = async (req, res) => {
-    try{
-        // check if the token is provided in the request parameter
+    try {
+        // Extract the token from the request parameter
         const { token } = req.params;
-        // if the token is not provided send a 400 bad request message
-       if (!token) {
-        return res.status(400).send('No token provided.');
-      }
 
-    //    if the token is provided proceed to decode the token with the secretKey to identify the user
+        // If the token is not provided, send a 400 bad request message
+        if (!token) {
+            return res.status(400).send('No token provided.');
+        }
+
+        // Decode the token with the secretKey to identify the user
         const decoded = jwt.verify(token, secretKey);
         const email = decoded.email;
 
-        // Confirm the subscription in the database and update confirm to true
+        // Find the subscriber by email
+        const subscriber = await Subscriber.findOne({ email });
+
+        // Check if the subscriber exists
+        if (!subscriber) {
+            return res.status(404).json({ message: 'Subscriber not found.' });
+        }
+
+        // Check if the subscription is already confirmed
+        if (subscriber.confirmed === true) {
+            // If already confirmed, redirect to success page
+            return res.redirect('https://furniro-iota-eight.vercel.app/#/newsLetter-succes');
+        }
+
+        // Confirm the subscription in the database and update confirmed to true
         await Subscriber.updateOne({ email }, { $set: { confirmed: true } });
 
-        // send a 200 success messeage
-        res.status(200).send(`Subscription confirmed for ${email}`);
+        // After updating, redirect to the success page
+        res.redirect('https://furniro-iota-eight.vercel.app/#/newsLetter-succes');
     } catch (error) {
         res.status(500).send('Invalid or expired token.' + error.message);
     }
