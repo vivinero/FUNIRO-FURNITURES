e1dbca63ebd98ecb3eb7a2384d44de10a52e3cbf
AdekunleM <adekunlemichael1319@gmail.com>
2024-10-11 03:36:09 +0100

    Removed authentication on filter

--- routers/filterRouter.js
+++ routers/filterRouter.js
@@ -6,7 +6,7 @@ const { authenticate } = require('../middleWares/authentication');
 const filterRouter = express.Router();
 
 // Define the route with the middleware and controller
-filterRouter.get('/filter', authenticate, filterProduct);
+filterRouter.get('/filter',  filterProduct);
 // how your url should look like
 // Example: http://localhost:5000/filter?productName=Shirt&categoryName=Apparel
 
